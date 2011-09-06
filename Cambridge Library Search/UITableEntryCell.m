e5a8b7ecd7d449e9535f09a7373a8273cb7794f4
James Snee <james.snee@googlemail.com>
2011-09-06 13:12:31 +0100

    Ignore updated and removed UITableEntryCell.m

---
+++ Cambridge Library Search/UITableEntryCell.m
@@ -0,0 +1,51 @@
+//
+//  UITableEntryCell.m
+//  Cambridge Library Search
+//
+//  Created by James Snee on 18/08/2011.
+//  Copyright 2011 James Snee. All rights reserved.
+//
+
+#import "UITableEntryCell.h"
+
+@implementation UITableEntryCell
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
