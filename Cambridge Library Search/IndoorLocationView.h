17dcec37e7bb240f66dfa9c78246ddb9073df8f8
James Snee <james.snee@googlemail.com>
2011-10-07 08:08:15 +0100

    Added IndoorLocationView controller and view.

---
+++ Cambridge Library Search/IndoorLocationView.h
@@ -0,0 +1,13 @@
+//
+//  IndoorLocationView.h
+//  Cambridge Library Search
+//
+//  Created by James Snee on 07/10/2011.
+//  Copyright 2011 James Snee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IndoorLocationView : UIViewController
+
+@end
---
+++ Cambridge Library Search/IndoorLocationView.m
@@ -0,0 +1,51 @@
+//
+//  IndoorLocationView.m
+//  Cambridge Library Search
+//
+//  Created by James Snee on 07/10/2011.
+//  Copyright 2011 James Snee. All rights reserved.
+//
+
+#import "IndoorLocationView.h"
+
+@implementation IndoorLocationView
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
